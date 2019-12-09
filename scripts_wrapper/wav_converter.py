0ebafdc8ff6e9bfcc6d91ca7ab66c5c89d78ab1c
Chimezie Iwuanyanwu <mr.chrisopher@yahoo.com>
2019-12-09 01:15:27 -0600

    added multiprocessing and timing

--- scripts_wrapper/wav_converter.py
+++ scripts_wrapper/wav_converter.py
@@ -5,9 +5,10 @@ from os.path import join
 import os
 import subprocess
 import re
+import time
 
 
-def __convert_file(input_path, file_name, output_path, verbose):
+def __convert_file(input_path, output_path, verbose, file_name):
     try:
         new_file_name = re.sub("[^a-z0-9-_]", "", get_filename(file_name, False))
         if verbose:
@@ -21,6 +22,7 @@ def __convert_file(input_path, file_name, output_path, verbose):
 
 
 def wav_converter(input_path, output_path, verbose_int=0):
+    start_time = time.time()
     if verbose_int not in [0, 1]:
         raise ValueError('%s is not a valid verbose input. Choose 0 for none or 1 for full' % verbose_int)
     verbose = True if verbose_int == 1 else False
@@ -34,13 +36,26 @@ def wav_converter(input_path, output_path, verbose_int=0):
             print("Starting .wav conversion\n-----------------------------------------")
 
         if os.path.isfile(input_path):
-            __convert_file(os.path.dirname(input_path), get_filename(input_path), output_path, verbose)
+            __convert_file(os.path.dirname(input_path), output_path, verbose, get_filename(input_path))
         else:
             file_names = get_filenames_from_folder(input_path)
-            for file_name in file_names:
-                __convert_file(input_path, file_name, output_path, verbose)
+            try:
+                import multiprocessing, psutil
+                from functools import partial
+                with multiprocessing.Pool(psutil.cpu_count(logical=False)) as pool:
+                    pool.map_async(partial(__convert_file, input_path, output_path, verbose), file_names).get()
+            except Exception:
+                if verbose:
+                    print("Cannot use multiprocessing. Defaulting to normal processing.")
+
+                for file_name in file_names:
+                    __convert_file(input_path, output_path, verbose, file_name)
+
     else:
         raise RuntimeError('Audio file(s) path %s not found' % input_path)
+    end_time = time.time()
+    if verbose:
+        print("Elapsed time was %g seconds" % (end_time - start_time))
 
 
 if __name__ == '__main__':
