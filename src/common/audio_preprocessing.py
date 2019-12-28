4d3831a6b9df114a865142b941064e28f4cc46fa
Chimezie Iwuanyanwu <mr.chrisopher@yahoo.com>
2019-12-28 09:57:56 -0600

    added audio_processing tests

--- src/common/audio_preprocessing.py
+++ src/common/audio_preprocessing.py
@@ -8,6 +8,7 @@ from madmom.features.beats import DBNBeatTrackingProcessor, RNNBeatProcessor
 from madmom.processors import SequentialProcessor, ParallelProcessor
 
 from common.Fprev_sub import Fprev_sub
+from configuration.parameters import MULTI_CHANNEL_FRAME_SIZES, SINGLE_CHANNEL_FRAME_SIZE
 
 
 def get_feature_processors(sample_rate, hopsize_t, frame_size):
@@ -20,31 +21,26 @@ def get_feature_processors(sample_rate, hopsize_t, frame_size):
     return SequentialProcessor([frames, stft, filt, spec])
 
 
-def get_madmom_melbank(file_name, sample_rate, hopsize_t, multi):
+def get_madmom_log_mels(file_name, sample_rate, hopsize_t, multi):
+    def nbf_2D(features, nlen):
+        features = np.array(features).transpose()
+        mfcc_out = np.array(features, copy=True)
+        for ii in range(1, nlen + 1):
+            mfcc_right_shift = Fprev_sub(features, w=ii)
+            mfcc_left_shift = Fprev_sub(features, w=-ii)
+            mfcc_out = np.vstack((mfcc_right_shift, mfcc_out, mfcc_left_shift))
+        features = mfcc_out.transpose()
+        return features
+
     sig = SignalProcessor(num_channels=1, sample_rate=sample_rate)
     if multi:
         multi_proc = ParallelProcessor([get_feature_processors(sample_rate, hopsize_t, frame_size)
-                                        for frame_size in [1024, 2048, 4096]])
-        return SequentialProcessor([sig, multi_proc, np.dstack])(file_name)
+                                        for frame_size in MULTI_CHANNEL_FRAME_SIZES])
+        mfcc = SequentialProcessor([sig, multi_proc, np.dstack])(file_name)
     else:
-        frame_size = 2048
-        single_proc = get_feature_processors(sample_rate, hopsize_t, frame_size)
-        return SequentialProcessor([sig, single_proc])(file_name)
+        single_proc = get_feature_processors(sample_rate, hopsize_t, SINGLE_CHANNEL_FRAME_SIZE)
+        mfcc = SequentialProcessor([sig, single_proc])(file_name)
 
-
-def nbf_2D(mfcc, nlen):
-    mfcc = np.array(mfcc).transpose()
-    mfcc_out = np.array(mfcc, copy=True)
-    for ii in range(1, nlen + 1):
-        mfcc_right_shift = Fprev_sub(mfcc, w=ii)
-        mfcc_left_shift = Fprev_sub(mfcc, w=-ii)
-        mfcc_out = np.vstack((mfcc_right_shift, mfcc_out, mfcc_left_shift))
-    feature = mfcc_out.transpose()
-    return feature
-
-
-def get_madmom_log_mels(file_name, sample_rate, hopsize_t, multi):
-    mfcc = get_madmom_melbank(file_name, sample_rate, hopsize_t, multi)
     if multi:
         mfcc_conc = [nbf_2D(mfcc[:, :, i], 7) for i in range(3)]
         return np.stack(mfcc_conc, axis=2)
@@ -52,7 +48,7 @@ def get_madmom_log_mels(file_name, sample_rate, hopsize_t, multi):
         return nbf_2D(mfcc, 7)
 
 
-def get_librosa_features(file_name, sample_rate, hopsize_t):
+def get_librosa_frames(file_name, sample_rate, hopsize_t):
     samples, _ = librosa.load(file_name, sr=sample_rate)
     onset_times = librosa.onset.onset_detect(y=samples,
                                              sr=sample_rate,
@@ -61,7 +57,7 @@ def get_librosa_features(file_name, sample_rate, hopsize_t):
     return np.array(np.around(np.array(onset_times) / hopsize_t), dtype=int)
 
 
-def get_madmom_features(file_name, hopsize_t):
+def get_madmom_frames(file_name, hopsize_t):
     proc = DBNBeatTrackingProcessor(max_bpm=300,
                                     fps=int(1 / hopsize_t))
     act = RNNBeatProcessor()
@@ -73,10 +69,10 @@ def get_madmom_features(file_name, hopsize_t):
 def get_madmom_librosa_features(file_name, sample_rate, hopsize_t, num_frames, frame_start=0):
     # TODO: add ability to choose which features to add
     # librosa features
-    librosa_frames = get_librosa_features(file_name, sample_rate, hopsize_t)
+    librosa_frames = get_librosa_frames(file_name, sample_rate, hopsize_t)
 
     # madmom features
-    madmom_frames = get_madmom_features(file_name, hopsize_t)
+    madmom_frames = get_madmom_frames(file_name, hopsize_t)
 
     # fill in blanks and return
     librosa_features = np.zeros((num_frames,))
--- tst/test_setup.py
+++ tst/test_setup.py
@@ -2,4 +2,5 @@ import os
 import sys
 
 myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(myPath + '/../src/'))
+sys.path.insert(0, os.path.join(myPath + '/../'))
+sys.path.insert(1, os.path.join(myPath + '/../src/'))
