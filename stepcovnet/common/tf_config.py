3a37e6b8e1352710ff88c634845ff5a097426601
Chimezie Iwuanyanwu <mr.chrisopher@yahoo.com>
2020-08-13 01:01:10 -0500

    Disabling Jit until compatability issues are fixed

--- stepcovnet/common/tf_config.py
+++ stepcovnet/common/tf_config.py
@@ -11,7 +11,7 @@ tf.config.experimental.set_memory_growth(gpu[0], True)
 # policy = tf.keras.mixed_precision.experimental.Policy('mixed_float16')
 # tf.keras.mixed_precision.experimental.set_policy(policy)
 
-tf.config.optimizer.set_jit(True)
+# tf.config.optimizer.set_jit(True)
 
 tf.random.set_seed(42)
 tf.compat.v1.set_random_seed(42)
