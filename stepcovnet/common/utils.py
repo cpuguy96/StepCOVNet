utils.py: Fixed bug where mask used wrong value; removed parallel processing of channel scalers AbstractConfig.py: Let arrow input and mask return undefined shape InferenceExecutor.py: Corrected arrow mask value InferenceInput.py: Set the intial mask padding to 0 TrainingInput.py: Added tokenizer support TrainingFeatureGenerator.py: Refactored; added support for tokenizer; fixed bug where arrow mask values were incorrect TrainingHyperparameters.py: Added support for tokenizer and set default tokenizer to pretrained GPT2

stepcovnet/common/utils.py
@@ -65,14 +65,9 @@ def get_channel_scalers(features, existing_scalers=None, n_jobs=1):
         features = feature_reshape_down(features=features)
 
     num_channels = features.shape[-1]
-
-    if existing_scalers is None:
-        channel_scalers = [StandardScaler() for _ in range(num_channels)]
-    else:
-        channel_scalers = existing_scalers
-    channel_scalers = Parallel(backend="loky", n_jobs=n_jobs)(
-        delayed(scaler.partial_fit)(features[:, :, i]) for i, scaler in
-        enumerate(channel_scalers))
+    channel_scalers = existing_scalers if existing_scalers is not None \
+        else [StandardScaler() for _ in range(num_channels)]
+    channel_scalers = [scaler.partial_fit(features[:, :, i]) for i, scaler in enumerate(channel_scalers)]
 
     return channel_scalers
 
@@ -212,13 +207,14 @@ def get_ngram(data, lookback, padding_value=0):
     return np.asarray(list(ngrams(data_w_padding, lookback)))
 
 
-def get_samples_ngram_with_mask(samples, lookback, squeeze=True, reshape=False):
+def get_samples_ngram_with_mask(samples, lookback, squeeze=True, reshape=False, sample_padding_value=0,
+                                mask_padding_value=1):
     if reshape:
-        ngram_samples = get_ngram(samples.reshape(-1, 1), lookback)
+        ngram_samples = get_ngram(samples.reshape(-1, 1), lookback, padding_value=sample_padding_value)
     else:
         ngram_samples = get_ngram(samples, lookback)
-    mask = np.zeros((samples.shape[0], 1), dtype=int)
-    ngram_mask = get_ngram(mask, lookback, padding_value=1)
+    mask = np.ones((samples.shape[0], 1), dtype=np.int32)
+    ngram_mask = get_ngram(mask, lookback, padding_value=mask_padding_value)
 
     if squeeze:
         ngram_samples = np.squeeze(ngram_samples)

stepcovnet/executor/InferenceExecutor.py
@@ -45,7 +45,7 @@ class InferenceExecutor(AbstractExecutor):
             arrow_input = np.roll(arrow_input, -1, axis=0)
             arrow_mask = np.roll(arrow_mask, -1, axis=0)
             arrow_input[0][-1] = self.label_arrow_encoder.encode(arrows)
-            arrow_mask[0][-1] = 0
+            arrow_mask[0][-1] = 1
             if self.verbose and audio_features_index % 100 == 0:
                 print("[%d/%d] Samples generated" % (audio_features_index, len(input_data.audio_features)))
         return pred_arrows

stepcovnet/inputs/InferenceInput.py
@@ -14,6 +14,7 @@ class InferenceInput(AbstractInput):
                                                  config=self.config.dataset_config)
         self.arrow_input_init, self.arrow_mask_init = get_samples_ngram_with_mask(samples=np.array([0]),
                                                                                   lookback=self.config.lookback,
-                                                                                  reshape=True)
+                                                                                  reshape=True,
+                                                                                  mask_padding_value=0)
         self.arrow_input_init = self.arrow_input_init[:-1, 1:]
         self.arrow_mask_init = self.arrow_mask_init[:-1, 1:]

stepcovnet/inputs/TrainingInput.py
@@ -30,7 +30,8 @@ class TrainingInput(AbstractInput):
                                                                 num_samples=self.config.num_train_samples,
                                                                 scalers=self.config.train_scalers,
                                                                 difficulty=self.config.difficulty,
-                                                                warmup=True)
+                                                                warmup=True,
+                                                                tokenizer=self.config.hyperparameters.tokenizer)
         self.val_feature_generator = TrainingFeatureGenerator(dataset_path=self.config.dataset_path,
                                                               dataset_type=self.config.dataset_type,
                                                               lookback=self.config.lookback,
@@ -39,7 +40,8 @@ class TrainingInput(AbstractInput):
                                                               num_samples=self.config.num_val_samples,
                                                               scalers=self.config.train_scalers,
                                                               difficulty=self.config.difficulty,
-                                                              shuffle=False)
+                                                              shuffle=False,
+                                                              tokenizer=self.config.hyperparameters.tokenizer)
         self.all_feature_generator = TrainingFeatureGenerator(dataset_path=self.config.dataset_path,
                                                               dataset_type=self.config.dataset_type,
                                                               lookback=self.config.lookback,
@@ -48,7 +50,8 @@ class TrainingInput(AbstractInput):
                                                               num_samples=self.config.num_samples,
                                                               scalers=self.config.all_scalers,
                                                               difficulty=self.config.difficulty,
-                                                              warmup=True)
+                                                              warmup=True,
+                                                              tokenizer=self.config.hyperparameters.tokenizer)
 
     def get_tf_dataset(self, generator):
         return tf.data.Dataset.from_generator(

stepcovnet/training/TrainingFeatureGenerator.py
@@ -8,7 +8,7 @@ from stepcovnet.common.utils import get_samples_ngram_with_mask
 
 class TrainingFeatureGenerator(object):
     def __init__(self, dataset_path, dataset_type, batch_size, indexes, num_samples, lookback=1, scalers=None,
-                 difficulty="challenge", warmup=False, shuffle=True):
+                 difficulty="challenge", warmup=False, shuffle=True, tokenizer=None):
         self.dataset_path = dataset_path
         self.dataset_type = dataset_type
         self.train_indexes = indexes
@@ -18,6 +18,8 @@ class TrainingFeatureGenerator(object):
         self.batch_size = batch_size
         self.difficulty = difficulty
         self.shuffle = shuffle
+        # Removing use of tokenizer until we can supply varied input feature sizes
+        self.tokenizer = None  # tokenizer
 
         # The Tensorflow calls the generator three times before starting a training job. We will "warmup" the data
         # yielding by returning the same data for the three calls. This way the indexing is aligned correctly.
@@ -33,12 +35,11 @@ class TrainingFeatureGenerator(object):
             dataset.set_difficulty(self.difficulty)
             self.song_index = 0
             self.song_start_index = None
+            new_song = True
             if self.shuffle:
                 self.rng.shuffle(self.train_indexes)
             while True:
                 features = defaultdict(lambda: np.array([]))
-                y_batch = None
-                sample_weights_batch = None
                 if self.song_index >= len(self.train_indexes):
                     self.song_index = 0
                 song_start_index, song_end_index = dataset.song_index_ranges[self.train_indexes[self.song_index]]
@@ -48,66 +49,94 @@ class TrainingFeatureGenerator(object):
                     self.warmup_countdown = max(self.warmup_countdown - 1, 0)
                 # We only return partial batches when at the end of the training data. Otherwise, use start of next song
                 # to append data to the batch.
-                while y_batch is None or self.song_index < len(self.train_indexes):
+                while len(features["y_batch"]) == 0 or self.song_index < len(self.train_indexes):
                     start_index = self.song_start_index
-                    y_batch_len = 0 if y_batch is None else len(y_batch)
+                    y_batch_len = len(features["y_batch"])
                     end_index = min(start_index + self.batch_size - y_batch_len, song_end_index)
 
-                    arrow_features, arrow_mask = get_samples_ngram_with_mask(
-                        dataset.label_encoded_arrows[start_index:end_index], self.lookback, reshape=True)
-                    audio_features, _ = get_samples_ngram_with_mask(dataset.features[start_index:end_index],
-                                                                    self.lookback, squeeze=False)
                     # Lookback data from ngram returns empty value in index 0. Also, arrow features should only
                     # contain previously seen features. Therefore, removing last element and last lookback from
                     # arrows features and first element from audio features.
-                    arrow_features = arrow_features[:-1, 1:]
-                    arrow_mask = arrow_mask[:-1, 1:]
+                    mask_padding_value = 0 if new_song else 1
+                    if self.tokenizer is not None:
+                        string_arrows = dataset.string_arrows[start_index:end_index]
+                        arrow_features, arrow_mask = get_samples_ngram_with_mask(string_arrows, self.lookback,
+                                                                                 reshape=True,
+                                                                                 sample_padding_value='0000',
+                                                                                 mask_padding_value=mask_padding_value)
+                        decoded_arrows = [" ".join(line) for line in arrow_features]
+                        arrow_features = [self.tokenizer(line, return_tensors='tf',
+                                                         add_prefix_space=True)['input_ids']
+                                              .numpy()[0][1:].astype(np.int32)
+                                          for line in decoded_arrows]
+                        arrow_features = arrow_features[:-1]
+                        arrow_mask = list(arrow_mask[:-1, 1:].astype(np.int32))
+                        for i in range(len(arrow_mask)):
+                            len_diff = len(arrow_features[i]) - len(arrow_mask[i])
+                            if len_diff > 0:
+                                arrow_mask[i] = np.concatenate((arrow_mask[i], np.zeros((len_diff,))), axis=0)
+                            elif len_diff < 0:
+                                arrow_mask[i] = arrow_mask[i][:-len_diff]
+                    else:
+                        encoded_arrows = dataset.label_encoded_arrows[start_index:end_index]
+                        arrow_features, arrow_mask = get_samples_ngram_with_mask(encoded_arrows, self.lookback,
+                                                                                 reshape=True,
+                                                                                 mask_padding_value=mask_padding_value)
+                        arrow_features = arrow_features[:-1, 1:].astype(np.int32)
+                        arrow_mask = arrow_mask[:-1, 1:].astype(np.int32)
+                    audio_features, _ = get_samples_ngram_with_mask(dataset.features[start_index:end_index],
+                                                                    self.lookback, squeeze=False)
                     audio_features = audio_features[1:]
                     arrows = dataset.binary_encoded_arrows[start_index: end_index]
                     sample_weights = dataset.sample_weights[start_index: end_index]
 
-                    # Append or set features/labels/sample weights based on if existing data is present
-                    if "arrow_features" in features:
-                        features["arrow_features"] = np.concatenate((features["arrow_features"], arrow_features),
-                                                                    axis=0)
-                    else:
-                        features["arrow_features"] = arrow_features
-                    if "arrow_mask" in features:
-                        features["arrow_mask"] = np.concatenate((features["arrow_mask"], arrow_mask), axis=0)
-                    else:
-                        features["arrow_mask"] = arrow_mask
-                    if "audio_features" in features:
-                        features["audio_features"] = np.concatenate((features["audio_features"], audio_features),
-                                                                    axis=0)
-                    else:
-                        features["audio_features"] = audio_features
-                    if y_batch is not None:
-                        y_batch = np.concatenate((y_batch, arrows), axis=0)
-                    else:
-                        y_batch = arrows
-                    if sample_weights_batch is not None:
-                        sample_weights_batch = np.concatenate((sample_weights_batch, sample_weights), axis=0)
-                    else:
-                        sample_weights_batch = sample_weights
-
+                    features = self.append_existing_data(features=features, arrow_features=arrow_features,
+                                                         arrow_mask=arrow_mask, audio_features=audio_features,
+                                                         arrows=arrows, sample_weights=sample_weights)
                     self.song_start_index = end_index
                     # Break if collected enough data for a batch or end of song list.
                     # Otherwise, change to next song to collect more.
-                    if len(y_batch) >= self.batch_size or self.song_index + 1 >= len(self.train_indexes):
+                    if len(features["y_batch"]) >= self.batch_size or self.song_index + 1 >= len(self.train_indexes):
+                        new_song = False
                         break
                     else:
                         self.song_index += 1
-                        song_start_index, song_end_index = dataset.song_index_ranges[
-                            self.train_indexes[self.song_index]]
+                        song_start_index, song_end_index = \
+                            dataset.song_index_ranges[self.train_indexes[self.song_index]]
                         self.song_start_index = song_start_index
+                        new_song = True
 
                 if self.song_start_index >= song_end_index:
                     self.song_index += 1
 
-                if y_batch is not None:
+                if len(features["y_batch"]) > 0:
                     scaled_audio_features = apply_timeseries_scalers(features=features["audio_features"],
                                                                      scalers=self.scalers)
-                    x_batch = {"arrow_input": features["arrow_features"].astype(np.int32),
-                               "arrow_mask": features["arrow_mask"].astype(np.int32),
+                    x_batch = {"arrow_input": features["arrow_features"],
+                               "arrow_mask": features["arrow_mask"],
                                "audio_input": scaled_audio_features.astype(np.float64)}
-                    yield x_batch, y_batch, sample_weights_batch
+                    yield x_batch, features["y_batch"], features["sample_weights_batch"]
+
+    @staticmethod
+    def append_existing_data(features, arrow_features, arrow_mask, audio_features, arrows,
+                             sample_weights):
+        # Append or set features/labels/sample weights based on if existing data is present
+        if not features or any(len(value) == 0 for value in features.values()):
+            features["arrow_features"] = arrow_features
+            features["arrow_mask"] = arrow_mask
+            features["audio_features"] = audio_features
+            features["y_batch"] = arrows
+            features["sample_weights_batch"] = sample_weights
+        else:
+            if isinstance(features["arrow_features"], list) or isinstance(features["arrow_mask"], list):
+                features["arrow_features"].append(arrow_features)
+                features["arrow_mask"].append(arrow_mask)
+            else:
+                features["arrow_features"] = np.concatenate((features["arrow_features"], arrow_features), axis=0)
+                features["arrow_mask"] = np.concatenate((features["arrow_mask"], arrow_mask), axis=0)
+            features["audio_features"] = np.concatenate((features["audio_features"], audio_features), axis=0)
+            features["y_batch"] = np.concatenate((features["y_batch"], arrows), axis=0)
+            features["sample_weights_batch"] = np.concatenate((features["sample_weights_batch"], sample_weights),
+                                                              axis=0)
+
+        return features