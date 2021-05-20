32cedd7cc7031d36f76c60a0fe96fdad84aa34ec
Chimezie Iwuanyanwu <mr.chrisopher@yahoo.com>
2021-05-20 13:34:11 -0500

    Fixed setting GPU growth config

--- stepcovnet/common/tf_config.py
+++ stepcovnet/common/tf_config.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 
 import tensorflow as tf
 
-tf.config.experimental.set_memory_growth(tf.config.experimental.list_physical_devices('GPU')[0], True)
+tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('GPU')[0], True)
 
 MIXED_PRECISION_POLICY = tf.keras.mixed_precision.Policy('mixed_float16')
 # Disabling global mixed precision since there is a bug in huggingface when using mixed precision in Tensorflow
