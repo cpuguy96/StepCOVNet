5e5f58203ff7493fe845f6a3bf375c68e00c5337
Chimezie Iwuanyanwu <mr.chrisopher@yahoo.com>
2023-12-02 14:25:59 -0600

    Migrated to Keras 3.0.0

--- stepcovnet/model/SimpleAudioModel.py
+++ stepcovnet/model/SimpleAudioModel.py
@@ -1,5 +1,4 @@
 from keras import layers
-from tensorflow.keras.layers import Layer
 
 from stepcovnet.config.TrainingConfig import TrainingConfig
 from stepcovnet.model.AudioModel import AudioModel
@@ -8,5 +7,5 @@ from stepcovnet.model.AudioModel import AudioModel
 class SimpleAudioModel(AudioModel):
     def _create_audio_model(
         self, training_config: TrainingConfig, model_input: layers.Input
-    ) -> Layer:
+    ) -> layers.Layer:
         raise NotImplementedError
