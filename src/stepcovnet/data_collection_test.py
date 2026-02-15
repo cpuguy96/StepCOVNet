9764c67fb5fd236cf060e125e51263ea3f6f96e2
Chimezie Iwuanyanwu <mr.chrisopher@yahoo.com>
2026-02-14 19:49:36 -0800

    Use absolute import in data_collection_test.py
    Fixes
    ImportError: attempted relative import beyond top-level package

--- src/stepcovnet/data_collection_test.py
+++ src/stepcovnet/data_collection_test.py
@@ -3,7 +3,7 @@ import unittest
 
 import numpy as np
 
-from ..stepcovnet import data_collection
+from src.stepcovnet import data_collection
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'testdata')
 
