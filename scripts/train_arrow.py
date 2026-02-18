9521e4c0f2bd0da032c6d1b29e08f4de238504e6
Chimezie Iwuanyanwu <mr.chrisopher@yahoo.com>
2026-02-17 21:12:54 -0800

    Add script for training arrow detection model
    This diff adds `scripts/train_arrow.py`, a command-line script to facilitate the training of the arrow detection model.
    
    Key changes:
    *   **Argument Parsing**: Configures `argparse` to accept necessary training parameters such as data directories (`--train_data_dir`, `--val_data_dir`), training duration (`--epochs`), and output locations (`--callback_root_dir`, `--model_output_dir`).
    *   **Training Execution**: Calls `trainers.run_arrow_train` with the parsed arguments, initializing the training process with a default batch size of 1 and normalization enabled.

---
+++ scripts/train_arrow.py
@@ -0,0 +1,76 @@
+r"""Script for training the arrow detection model.
+
+Usage:
+    python scripts/train_arrow.py --train_data_dir=/path/to/train/data --val_data_dir=/path/to/val/data --epochs=20 --callback_root_dir=/path/to/callbacks --model_output_file=/path/to/save/model.keras
+"""
+
+import argparse
+
+from stepcovnet import trainers
+
+PARSER = argparse.ArgumentParser(description="Train arrow detection model.")
+PARSER.add_argument(
+    "--train_data_dir",
+    type=str,
+    help="Directory containing training data.",
+    default=None,
+    required=True,
+)
+PARSER.add_argument(
+    "--val_data_dir",
+    type=str,
+    help="Directory containing validation data.",
+    default=None,
+    required=True,
+)
+PARSER.add_argument(
+    "--epochs",
+    type=int,
+    help="Number of epochs to train for.",
+    default=10,
+    required=False,
+)
+PARSER.add_argument(
+    "--callback_root_dir",
+    type=str,
+    help="Root directory for storing training callbacks (checkpoints, logs).",
+    default=None,
+    required=True,
+)
+PARSER.add_argument(
+    "--model_output_dir",
+    type=str,
+    help="Directory where the trained model will be saved.",
+    default=None,
+    required=True,
+)
+PARSER.add_argument(
+    "--take_count",
+    type=int,
+    help="Number of batches to use from the training dataset.",
+    default=1,
+    required=False,
+)
+
+ARGS = PARSER.parse_args()
+
+
+def main():
+    batch_size = 1
+    normalize = True
+
+    trainers.run_arrow_train(
+        data_dir=ARGS.train_data_dir,
+        val_data_dir=ARGS.val_data_dir,
+        batch_size=batch_size,
+        normalize=normalize,
+        model_params={},
+        take_count=ARGS.take_count,
+        epoch=ARGS.epochs,
+        callback_root_dir=ARGS.callback_root_dir,
+        model_output_dir=ARGS.model_output_dir,
+    )
+
+
+if __name__ == "__main__":
+    main()
